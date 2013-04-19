8d99d2b1ff9649e05fd13c177ab680b1e7c0e296
Adam Anderson <horatio.blackwood@gmail.com>
2013-04-19 16:47:29 -0700

    Added jdoc to PdfFont.java

--- src/plainpdf/PdfFont.java
+++ src/plainpdf/PdfFont.java
@@ -6,20 +6,23 @@ import org.apache.pdfbox.pdmodel.font.PDType1Font;
  * A list of fonts that can be used with TextToPdf.
  *
  * @author adam
- * @date Feb 6, 2013.
+ * @date April 15, 2013.
  */
 public enum PdfFont {
 
+    /** Helvetica-based fonts. */
     HELVETICA(PDType1Font.HELVETICA),
     HELVETICA_BOLD(PDType1Font.HELVETICA_BOLD),
     HELVETICA_ITALIC(PDType1Font.HELVETICA_OBLIQUE),
     HELVETICA_BOLD_ITALIC(PDType1Font.HELVETICA_BOLD_OBLIQUE),
 
+    /** Courier-based fonts. */
     COURIER(PDType1Font.COURIER),
     COURIER_BOLD(PDType1Font.COURIER_BOLD),
     COURIER_ITALIC(PDType1Font.COURIER_OBLIQUE),
     COURIER_BOLD_ITALIC(PDType1Font.COURIER_BOLD_OBLIQUE),
 
+    /** Times-based fonts. */
     TIMES(PDType1Font.TIMES_ROMAN),
     TIMES_BOLD(PDType1Font.TIMES_BOLD),
     TIMES_ITALIC(PDType1Font.TIMES_ITALIC),
